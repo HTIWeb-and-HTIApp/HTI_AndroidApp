53846317f250f9659cbda240dc1be05e48385f06
mahdy <mahdymoaz1@gmail.com>
2024-03-25 10:56:58 +0200

    Services Screen Design

---
+++ services/ui/src/main/java/com/example/servicesui/componante/ServicesContent.kt
@@ -0,0 +1,30 @@
+package com.example.servicesui.componante
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ServicesContent() {
+    Box(
+       modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ServiceTopBar()
+            Spacer(modifier = Modifier.height(12.dp))
+            ServicesLazyColumn()
+        }
+    }
+}
